3cdc0d7b43f11314ed2d1f2796ef772d8a3cc4d1
MDmazen <mazena083@gmail.com>
2025-01-08 19:37:23 +0200

    Update ShareModal.js

--- src/components/modals/ShareModal.js
+++ src/components/modals/ShareModal.js
@@ -23,7 +23,7 @@ function ShareMovieModal({ modalOpen, setModalOpen, movie }) {
       shareButton: WhatsappShareButton,
     },
   ];
-  const url = `${window.location.protocol}//${window.location.host}/movie/${movie.id}`;
+  const url = `${window.location.protocol}//${window.location.host}/movie/${movie.name}`;
   return (
     <MainModal modalOpen={modalOpen} setModalOpen={setModalOpen}>
       <div className="inline-block sm:w-4/5 border border-border md:w-3/5 lg:w-2/5 w-full align-middle p-10 overflow-y-auto h-full bg-main text-white rounded-2xl">
@@ -35,7 +35,7 @@ function ShareMovieModal({ modalOpen, setModalOpen, movie }) {
         </Typography>
         <form className="flex-rows flex-wrap gap-6 mt-6">
           {shareData.map((data, index) => (
-            <data.shareButton key={index} url={url}>
+            <data.shareButton key={index} url={url} >
               <div className="w-12 transitions hover:bg-subMain flex-colo text-lg h-12 bg-white rounded bg-opacity-30">
                 <data.icon />
               </div>
