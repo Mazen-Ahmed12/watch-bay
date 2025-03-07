9abc4f240b863298a9a86e3fff8a7280c0e020ef
mazen ahmed <46138799+Mazen-Ahmed12@users.noreply.github.com>
2025-03-07 19:23:16 +0200

    the deployment done

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const root = createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter  basename="/watch-bay">
       <App />
     </BrowserRouter>
   </React.StrictMode>
